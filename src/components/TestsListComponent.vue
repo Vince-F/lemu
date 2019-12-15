cd9871cb5bca5710d9a019663e1f4dbf54238f93
Vince <vincent.francolin@outlook.com>
2019-12-15 23:03:52 +0100

    Add confirmation modal and add confirmation before deleting tests

--- src/components/TestsListComponent.vue
+++ src/components/TestsListComponent.vue
@@ -81,6 +81,7 @@ import { Vue, Component } from "vue-property-decorator";
 import { Getter, Mutation } from "vuex-class";
 import { BackstopTest } from "../models/backstopTest";
 import TestViewComponent from "./TestViewComponent.vue";
+import { ConfirmationModalService } from "../services/confirmationModalService";
 
 @Component({
   name: "tests-list-component",
@@ -111,7 +112,12 @@ export default class TestsListComponent extends Vue {
   }
 
   private deleteTest(testIndex: number) {
-    this.removeScenario(testIndex);
+    ConfirmationModalService.launchConfirmationModal()
+      .then(() => {
+        this.removeScenario(testIndex);
+        this.selectedTest = null;
+        this.selectedIndex = null;
+      });
   }
 
   private duplicateTest(testIndex: number) {
