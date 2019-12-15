95d7ce533058cb230b2aec9587c2a16bc8d14384
Vince-F <vincent.francolin@outlook.com>
2019-12-15 12:14:54 +0100

    Revert "Feature/hide save when not loaded"

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <template>
   <v-app>
-    <app-toolbar-component></app-toolbar-component>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Lemu Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/icon.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <h1 class="app-title">LEMU</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text v-on:click="save">
+        <v-icon>mdi-content-save</v-icon>
+        Save
+      </v-btn>
+    </v-app-bar>
+
     <v-content>
       <router-view />
     </v-content>
@@ -16,13 +37,19 @@
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import {Action} from "vuex-class";
-import AppToolbarComponent from "./components/AppToolbarComponent.vue";
 
-@Component({
-  components: {
-    AppToolbarComponent
-  }
-})
+@Component
 export default class App extends Vue {
+  @Action("configurationStore/saveConfiguration")
+  private saveConfiguration!: () => Promise<void>;
+
+  private save() {
+    this.saveConfiguration()
+      .then(() => {
+        alert("File saved");
+      }).catch((error) => {
+        alert(error);
+      });
+  }
 }
 </script>
