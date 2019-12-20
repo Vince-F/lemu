11c5577769a822b2a0b90d7b86814fcc252c5fbf
Vince <vincent.francolin@outlook.com>
2019-12-20 23:35:13 +0100

    Add a confirmation modal to save/discard change before closing config

--- src/services/confirmationModalService.ts
+++ src/services/confirmationModalService.ts
@@ -1,21 +1,16 @@
 import ConfirmationModalComponent from "../components/modals/ConfirmationModalComponent.vue";
+import SaveConfirmationModalComponent from "../components/modals/SaveConfirmationModalComponent.vue";
 
 export class ConfirmationModalService {
-  public static launchConfirmationModal() {
+  public static launchConfirmationModal(): Promise<void> {
     return new Promise((resolve, reject) => {
-      let element = document.getElementById("#modal");
-      if (!element) {
-        element = document.createElement('div');
-        element.id = "modal";
-        document.getElementById("#app")
-        document.body.appendChild(element);
-      }
-
+      const element = this.getModalContainerElement();
       const modal = new ConfirmationModalComponent({
         parent: window.vueApp
       });
 
       modal.$on('accept', () => {
+        modal.$destroy();
         resolve();
       });
       modal.$on('dimiss', () => {
@@ -25,5 +20,40 @@ export class ConfirmationModalService {
 
       modal.$mount(element);
     });
+  }
+
+  public static launchSaveConfirmationModal(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const element = this.getModalContainerElement();
+      const modal = new SaveConfirmationModalComponent({
+        parent: window.vueApp
+      });
+
+      modal.$on('save', () => {
+        modal.$destroy();
+        resolve("save");
+      });
+      modal.$on('discard', () => {
+        modal.$destroy();
+        resolve("discard");
+      });
+      modal.$on('dimiss', () => {
+        modal.$destroy();
+        reject();
+      });
+
+      modal.$mount(element);
+    });
+  }
+
+  private static getModalContainerElement() {
+    let element = document.getElementById("#modal");
+    if (!element) {
+      element = document.createElement('div');
+      element.id = "modal";
+      document.getElementById("#app");
+      document.body.appendChild(element);
+    }
+    return element;
   }
 }
