3b32e418342558f00acbf0669ccc1f59b4d43ac0
Vince <vincent.francolin@outlook.com>
2020-01-02 23:38:29 +0100

    Use application level snackbar and display it infinite time

--- src/components/ReportComponent.vue
+++ src/components/ReportComponent.vue
@@ -23,23 +23,6 @@
         Tests are running...
       </div>
     </div>
-
-    <v-snackbar v-model="snackbarDisplayed">
-      <v-icon color="green" v-if="snackbarSuccess">
-        mdi-check-circle
-      </v-icon>
-      <v-icon color="red" v-else>
-        mdi-alert
-      </v-icon>
-      {{ snackbarText }}
-      <v-btn
-        color="white"
-        text
-        @click="snackbarDisplayed = false"
-      >
-        Close
-      </v-btn>
-    </v-snackbar>
   </div>
   
 </template>
@@ -54,7 +37,7 @@
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
-import { Action, State } from "vuex-class";
+import { Action, State, Mutation } from "vuex-class";
 import { BackstopConfiguration } from '../models/backstopConfiguration';
 import { FileService } from '../services/fileService';
 
@@ -67,21 +50,12 @@ export default class ReportComponent extends Vue {
   @State((state) => state.configurationStore.testRunning)
   private readonly testRunning!: boolean;
   @Action("configurationStore/approveTests")
-  private readonly approveTests!: () => Promise<any>; 
-
-  private snackbarDisplayed: boolean;
-  private snackbarText: string;
-  private snackbarSuccess: boolean;
-
-  constructor() {
-    super(arguments);
-    this.snackbarDisplayed = false;
-    this.snackbarText = "";
-    this.snackbarSuccess = false;
-  }
+  private readonly approveTests!: () => Promise<any>;
+  @Mutation("applicationStore/displaySnackbar")
+  private readonly displaySnackbar!: (payload: {text: string, success: boolean}) => void;
 
   private get reportPath() {
-    const configurationPath = this.configuration && this.configuration.paths && 
+    const configurationPath = this.configuration && this.configuration.paths &&
         this.configuration.paths.html_report || "";
     const prefixPath = this.path.substr(0, this.path.length - "backstop.json".length);
     return configurationPath &&
@@ -91,18 +65,14 @@ export default class ReportComponent extends Vue {
   private approveTestsResult() {
     this.approveTests()
       .then(() => {
-        this.snackbarDisplayed = true;
-        this.snackbarText = "Tests successfully approved.";
-        this.snackbarSuccess = true;
+        this.displaySnackbar({text: "Tests successfully approved.", success: true});
         const reportFrame = this.$refs.reportFrame;
         if (reportFrame instanceof HTMLIFrameElement && reportFrame
           && reportFrame.contentWindow) {
           reportFrame.contentWindow.location.reload(true);
         }
       }).catch((error) => {
-        this.snackbarDisplayed = true;
-        this.snackbarText = "Test approval failed. Error: " + error;
-        this.snackbarSuccess = false;
+        this.displaySnackbar({text: "Test approval failed. Error: " + error, success: false})
       });
   }
 }
