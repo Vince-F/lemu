3bf76e0df55f547e4c6667cfc839a74875630da7
Vince <vincent.francolin@outlook.com>
2020-01-04 20:29:15 +0100

    Stop overriding report and remove only browser to avoid opening report

--- mainProcess/src/controllers/backstopTestRunner.ts
+++ mainProcess/src/controllers/backstopTestRunner.ts
@@ -2,6 +2,12 @@ import backstop = require("backstopjs");
 
 export class BackstopTestRunner {
   public static runTest(config: any): Promise<any> {
+    if (Array.isArray(config.report)) {
+      const idx = config.report.indexOf("browser");
+      if (idx > -1) {
+        config.report.splice(idx, 1); // to prevent opening the browser
+      }
+    }
     return backstop('test', {
       config
     });
