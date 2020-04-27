4ee317c449db706bd0bc2e8be2455810947e5c36
Vince <vincent.francolin@outlook.com>
2020-04-27 23:13:38 +0200

    Change following CR

--- mainProcess/src/controllers/backstopWorker.ts
+++ mainProcess/src/controllers/backstopWorker.ts
@@ -1,25 +1,22 @@
-import { Worker, parentPort, workerData, isMainThread } from "worker_threads";
+import { parentPort } from "worker_threads";
 const backstop = require("backstopjs");
 
-if (isMainThread) {
-} else {
-  parentPort.on("message", (msg) => {
-    const command = msg.command;
-    const options = msg.options;
-  
-    backstop(command, options)
-      .then((...args: any[]) => {
-        parentPort.postMessage({
-          success: true,
-          arguments: args
-        });
-      }).catch((...args: any[]) => {
-        parentPort.postMessage({
-          success: false,
-          arguments: args
-        });
+parentPort.on("message", (msg) => {
+  const command = msg.command;
+  const options = msg.options;
+
+  backstop(command, options)
+    .then((...args: any[]) => {
+      parentPort.postMessage({
+        success: true,
+        arguments: args
       });
-  });
-}
+    }).catch((...args: any[]) => {
+      parentPort.postMessage({
+        success: false,
+        arguments: args
+      });
+    });
+});
 
 
