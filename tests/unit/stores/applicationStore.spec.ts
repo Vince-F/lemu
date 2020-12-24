c2b8ec7504b473fe43851152f50087072aa76707
Vince <vincent.francolin@outlook.com>
2020-12-24 17:10:07 +0100

    Add action test on applicationStore and fix action workflow

--- tests/unit/stores/applicationStore.spec.ts
+++ tests/unit/stores/applicationStore.spec.ts
@@ -1,5 +1,7 @@
 import { ApplicationInfo } from "@/models/applicationInfo";
 import applicationStore from "@/store/applicationStore";
+import store from "@/store/index";
+import {ActionContext, ActionHandler} from "vuex";
 
 describe("applicationStore", () => {
   describe("setSnackbarDisplayed", () => {
@@ -58,6 +60,30 @@ describe("applicationStore", () => {
       applicationStore.mutations?.fillAppInfos(state, appInfos);
 
       expect(state.appInfos).toStrictEqual(new ApplicationInfo(appInfos));
+    });
+  });
+
+  describe("displaySnackbar", () => {
+    it("should display the snackbar and hides it after 15 seconds", () => {
+      jest.useFakeTimers();
+      const commit = jest.fn();
+      const context: ActionContext<any, any> = {
+        dispatch: jest.fn(),
+        commit,
+        state: {},
+        getters: {},
+        rootState: {},
+        rootGetters: {}
+      };
+      (applicationStore.actions?.displaySnackbar as ActionHandler<any, any>)
+        .call(store, context, {text: "test", success: true});
+
+      expect(commit.mock.calls[0][0]).toBe("setSnackbarDisplayed");
+      expect(commit.mock.calls[0][1]).toEqual({text: "test", success: true});
+
+      jest.runAllTimers();
+
+      expect(commit.mock.calls[1][0]).toBe("hideSnackbar");
     });
   });
 });
